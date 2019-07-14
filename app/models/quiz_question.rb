86393d40b5c12e07c75e962f5832df2c32a59394
Jim <teachlife@outlook.com>
2019-07-14 18:43:46 -0400

    adds quiz_questions model

---
+++ app/models/quiz_question.rb
@@ -0,0 +1,4 @@
+class QuizQuestions < ActiveRecord::Base
+  belongs_to :quiz
+  belongs_to :question
+end
--- app/models/quizes.rb
+++ app/models/quizes.rb
@@ -2,4 +2,6 @@ class Quizes < ActiveRecord::Base
   belongs_to :teacher
   has_many :quiz_questions
   has_many :questions, through: :quiz_questions
+  has_many :course_quizzes
+  has_many :courses, through: :course_quizzes
 end
