1cccd9c788bf252eafd20512aa93cf9c602448f7
Jim <teachlife@outlook.com>
2019-07-14 18:12:58 -0400

    adds initial application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, "password privacy"
+    set :views, 'app/views'
+  end
+
+end
