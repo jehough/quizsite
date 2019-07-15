931b39b765e9637e459d6cc71143dc1e63b75098
Jim <teachlife@outlook.com>
2019-07-14 21:06:08 -0400

    adds courses and student courses table

---
+++ db/migrate/20190715010452_create_student_courses.rb
@@ -0,0 +1,8 @@
+class CreateStudentCourses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :student_courses do |t|
+      t.integer :student_id
+      t.integer :course_id
+    end
+  end
+end
