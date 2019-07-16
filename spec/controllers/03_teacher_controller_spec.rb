cee15c2abe3c22c4f5a81073dfc2f3ef0e70925a
Jim <teachlife@outlook.com>
2019-07-16 10:43:33 -0400

    adds test framework for teachers controller

--- spec/controllers/03_teacher_controller_spec.rb
+++ spec/controllers/03_teacher_controller_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 require 'pry'
 
 describe "Create Teacher" do
-  
+  it "shows a page with name and password options"
+
+  it "creates a new teacher when submitted"
+
+  it "redirects to the user homepage"
+
+end
+
+describe "Teacher Home Page" do
+  it "has links to teachers classes"
+
+  it "has links to quizes"
+
+  it "has links to questions"
+
+end
