6da4150121c0008d560da38baea9ec3652c335dd
Jim <teachlife@outlook.com>
2019-07-17 14:16:47 -0400

    adds quiz show page and add a quiz

--- app/controllers/teacher_quiz_controller.rb
+++ app/controllers/teacher_quiz_controller.rb
@@ -1,4 +1,32 @@
 class TeacherQuizController < ApplicationController
 
 
+  post '/teacher/:slug/quiz' do
+    if Helper.is_teacher?(session)
+      if (Helper.current_teacher(session) == Teacher.find_by_slug(params[:slug]))
+        @teacher = Helper.current_teacher(session)
+        quiz = Quiz.create(:name => params[:name], :teacher_id => @teacher.id)
+        redirect "/teacher/#{@teacher.slug}/quiz/#{quiz.id}"
+      else
+        redirect '/'
+      end
+    else
+      redirect '/'
+    end
+  end
+
+  get '/teacher/:slug/quiz/:id' do
+    if Helper.is_teacher?(session)
+      if (Helper.current_teacher(session) == Teacher.find_by_slug(params[:slug]))
+        @teacher = Helper.current_teacher(session)
+        @quiz = Quiz.find(params[:id])
+        erb :'quiz/teacher/show'
+      else
+        redirect '/'
+      end
+    else
+      redirect '/'
+    end
+  end
+
 end
