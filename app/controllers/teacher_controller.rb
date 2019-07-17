5ec24f7bc203517e3ff14acc0d43c722513a61ff
Jim <teachlife@outlook.com>
2019-07-16 21:20:03 -0400

    adds username flash message

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -1,5 +1,8 @@
 require 'pry'
+require 'rack-flash'
+
 class TeacherController < ApplicationController
+  use Rack::Flash
 
   get '/teacher' do
     @teacher = Teacher.find(session[:teacher_id])
@@ -14,6 +17,7 @@ class TeacherController < ApplicationController
     if (params[:name] == "" || params[:password] == "")
       redirect '/signup'
     elsif !!(Teacher.find_by(:name => params[:name]))
+      flash[:message] = "This username is taken."
       redirect '/teacher/new'
     else
       teacher = Teacher.create(:name => params[:name], :password =>[:password])
