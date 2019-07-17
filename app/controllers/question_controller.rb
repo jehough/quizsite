9c198aaea37c5b8ae999331418d4bee0abd662b3
Jim <teachlife@outlook.com>
2019-07-17 14:34:33 -0400

    adds framework for question controller

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class QuestionController < ApplicationController
+
+  get '/teacher/:slug/question' do
+
+  end
+
+  get '/teacher/:slug/question/new' do
+
+  end
+
+  post '/teacher/:slug/question' do
+
+  end
+
+  get '/teacher/:slug/question/:id/edit' do
+
+  end
+
+  get '/teacher/:slug/question/:id' do
+
+  end
+
+  patch '/teacher/:slug/question/:id' do
+    
+  end
+
+end
