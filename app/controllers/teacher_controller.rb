d484da4596dc2b2c03d62873d5f432bab0e99518
Jim <teachlife@outlook.com>
2019-07-17 09:01:04 -0400

    adds teacher show page

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -4,10 +4,6 @@ require 'rack-flash'
 class TeacherController < ApplicationController
   use Rack::Flash
 
-  get '/teacher' do
-    @teacher = Teacher.find(session[:teacher_id])
-    erb :'teacher/index'
-  end
 
   get '/teacher/new' do
     erb :'teacher/new'
@@ -22,13 +18,14 @@ class TeacherController < ApplicationController
     else
       teacher = Teacher.create(:name => params[:name], :password => params[:password])
       session[:teacher_id] = teacher.id
-      redirect '/teacher'
+      redirect "/teacher/#{teacher.slug}"
     end
   end
 
   get '/teacher/login' do
-    if Helper.is_teacher?
-      redirect '/teacher'
+    if Helper.is_teacher?(session)
+      teacher = Helper.current_teacher(session)
+      redirect "teacher/#{teacher.slug}"
     elsif Helper.is_student?
       flash[:message] = "Students may not log in as teachers"
       redirect '/'
@@ -41,10 +38,19 @@ class TeacherController < ApplicationController
     teacher = Teacher.find_by(:name =>params[:name])
     if teacher && teacher.authenticate(params[:password])
       session[:teacher_id]= teacher.id
-      redirect '/teacher'
+      redirect "/teacher/#{teacher.slug}"
     else
       flash[:message] = "Username/Password not found!"
       redirect '/teacher/login'
+    end
+  end
+
+  get '/teacher/:slug'
+    if Helper.is_teacher?(session)
+      @teacher = Helper.current_teacher(session)
+      erb :'teacher/show'
+    else
+      redirect '/'
     end
   end
 
