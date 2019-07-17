552321812877888d9a3ea4fb5e23f3ed3db40dbd
Jim <teachlife@outlook.com>
2019-07-17 15:09:07 -0400

    adds new question page

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -17,7 +17,17 @@ class QuestionController < ApplicationController
   end
 
   get '/teacher/:slug/question/new' do
-
+    if Helper.is_teacher?(session)
+      if (Helper.current_teacher(session) == Teacher.find_by_slug(params[:slug]))
+        @teacher = Helper.current_teacher(session)
+        @questions = @teacher.questions
+        erb :'questions/new'
+      else
+        redirect '/'
+      end
+    else
+      redirect '/'
+    end
   end
 
   post '/teacher/:slug/question' do
