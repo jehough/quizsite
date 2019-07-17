4d3b463e69944b377b661661508c01835de70eca
Jim <teachlife@outlook.com>
2019-07-16 21:30:35 -0400

    adds test for teacher login

--- spec/controllers/03_teacher_controller_spec.rb
+++ spec/controllers/03_teacher_controller_spec.rb
@@ -10,8 +10,6 @@ describe "Create Teacher" do
     expect(last_response.body).to include("Username:")
   end
 
-
-
   it "creates a new teacher when submitted" do
       visit "teacher/new"
       fill_in "Name", with: "Mr. Awesome"
@@ -31,4 +29,43 @@ describe "Create Teacher" do
     expect(body).to include("This username is taken")
   end
 
+end
+
+describe "Teacher Log-in" do
+
+  it "has space for username and password" do
+    get "/teacher/login"
+    expect(last_response.status).to eq(200)
+    expect(last_response.body).to include("Password:")
+    expect(last_response.body).to include("Username:")
+  end
+
+  it "logs in an existing user" do
+    Teacher.create(:name => "Mr. Awesome", :password => "password")
+    visit "teacher/login"
+    fill_in "Name", with: "Mr. Awesome"
+    fill_in "Password", with: "password"
+    click_on "Log In"
+
+    expect(body).to include("Welcome, Mr. Awesome!")
+  end
+
+  it "will not let student log into teacher" do
+    Student.create(:name => "Sam", :password => "jackinthebox")
+    visit "teacher/login"
+    fill_in "Name", with: "Sam"
+    fill_in "Password", with: "jackinthebox"
+    click_on "Log In"
+
+    expect(body).to include("Username/Password not found!")
+  end
+
+  it "will not let a non-user log-in" do
+    visit "teacher/login"
+    fill_in "Name", with: "Joe"
+    fill_in "Password", with: "notateacher"
+    click_on "Log In"
+
+    expect(body).to include("Username/Password not found!")
+  end
 end
