3b2d269d7c57911f02386459d0c7d0e5cce4d33f
Jim <teachlife@outlook.com>
2019-07-18 14:52:53 -0400

    tests for quiz route

---
+++ spec/controllers/05_student_quiz_controller.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+require 'pry'
+
+describe "Student Quiz Controller" do
+  @student = Student.create(:name => "Sally", :password => "password")
+  @student2 = Student.create(:name => "Joe", :password => "password")
+  @quiz = Quiz.create(:name => "first quiz")
+  course = Course.create(:name => "Chemistry")
+  course_quiz = CourseQuiz.create(:course_id => course.id, :quiz_id => @quiz.id)
+  @stquiz1 = StudentQuiz.create(:student_id => @student.id, :quiz_id => @quiz.id, :course_quiz => course_quiz.id, :open_time => Time.now + 3600, :close_time => (Time.now + 7200))
+  @stquiz2 = StudentQuiz.create(:student_id => @student.id, :quiz_id => @quiz.id, :course_quiz => course_quiz.id, :open_time => Time.now - 7200, :close_time => (Time.now - 3600))
+  before do
+    params = {:name => "Sally", :password => "password"}
+    post "/student/login", params
+  end
+
+  it "will not access the quiz if not in right timeframe" do
+    get "/student/#{@student.slug}/quiz/#{@stquiz1.id}"
+    expect(last_response.body).to include("not open")
+  end
+
+  it "will not access the quiz if not the right student" do
+    get "/student/#{@student2.slug}/quiz/#{@stquiz2.id}"
+    expect(last_response.body).to include("Hello")
+  end
+
+  it "will not allow a student to take the quiz again" do
+    @stquiz1.score = 90
+    get "/student/#{@student.slug}/quiz/#{@stquiz1.id}"
+    expect(last_response.body).to include("already taken")
+  end
+
+end
