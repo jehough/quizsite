a4a6aee00d17d73a394de5b8bf8592d95f6c4ecb
Jim <teachlife@outlook.com>
2019-07-18 11:05:45 -0400

    adds validation to quiz assignment

--- app/controllers/teacher_course_quiz_controller.rb
+++ app/controllers/teacher_course_quiz_controller.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'rack-flash'
 
 class TeacherCourseQuizController < ApplicationController
 
@@ -22,8 +23,13 @@ class TeacherCourseQuizController < ApplicationController
       if (Helper.current_teacher(session) == Teacher.find_by_slug(params[:slug]))
         @teacher = Helper.current_teacher(session)
         @course = Course.find(params[:id])
-        CourseQuiz.add_quiz_to_course(params)
-        redirect "/teacher/#{@teacher.slug}/course/#{@course.id}"
+        if (params[:quiz] == '' || params[:start_time]== ''||params[:end_time]=='')
+          flash[:message] = "You must fill in all fields"
+          redirect "/teacher/#{@teacher.slug}/course/#{@course.id}/quiz"
+        else
+          CourseQuiz.add_quiz_to_course(params)
+          redirect "/teacher/#{@teacher.slug}/course/#{@course.id}"
+        end
       else
         redirect '/'
       end
