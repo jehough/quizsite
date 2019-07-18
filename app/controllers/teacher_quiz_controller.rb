e528fa70a4b9295213aef4436677b7b4d042c5ac
Jim <teachlife@outlook.com>
2019-07-18 07:21:22 -0400

    adds delete button for quiz on main teacher page and adds edit page

--- app/controllers/teacher_quiz_controller.rb
+++ app/controllers/teacher_quiz_controller.rb
@@ -15,6 +15,34 @@ class TeacherQuizController < ApplicationController
     end
   end
 
+  get '/teacher/:slug/quiz/' do
+    if Helper.is_teacher?(session)
+      if (Helper.current_teacher(session) == Teacher.find_by_slug(params[:slug]))
+        @teacher = Helper.current_teacher(session)
+        erb :'quiz/teacher/index'
+      else
+        redirect '/'
+      end
+    else
+      redirect '/'
+    end
+  end
+
+  get '/teacher/:slug/quiz/:id/edit' do
+    if Helper.is_teacher?(session)
+      if (Helper.current_teacher(session) == Teacher.find_by_slug(params[:slug]))
+        @teacher = Helper.current_teacher(session)
+        @quiz = Quiz.find(params[:id])
+        @questions = Question.all
+        erb :'quiz/teacher/edit'
+      else
+        redirect '/'
+      end
+    else
+      redirect '/'
+    end
+  end
+
   get '/teacher/:slug/quiz/:id' do
     if Helper.is_teacher?(session)
       if (Helper.current_teacher(session) == Teacher.find_by_slug(params[:slug]))
@@ -27,6 +55,13 @@ class TeacherQuizController < ApplicationController
     else
       redirect '/'
     end
+  end
+
+  delete '/teacher/:slug/quiz/:id' do
+    teacher = Helper.current_teacher(session)
+    quiz = Quiz.find(params[:id])
+    quiz.destroy
+    redirect "/teacher/#{teacher.slug}"
   end
 
 end
