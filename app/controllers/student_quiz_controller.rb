821af18d74349b9e395475232285c065d4d2c29b
Jim <teachlife@outlook.com>
2019-07-18 14:23:51 -0400

    sets up controller to show quiz for students

--- app/controllers/student_quiz_controller.rb
+++ app/controllers/student_quiz_controller.rb
@@ -1,5 +1,28 @@
 require 'pry'
+require 'date'
 
 class StudentQuizController < ApplicationController
-  
+
+  get '/student/:slug/quiz/:id' do
+    if Helper.is_student?(session)
+      if (Helper.current_student(session) == Student.find_by_slug(params[:slug]))
+        @student = Helper.current_student(session)
+        @stquiz = StudentQuiz.find(params[:id])
+        if (@stquiz.score != nil)
+          flash[:message] = "I'm sorry #{@student.name}, you have already taken this quiz"
+          erb :'/quiz/student/error'
+        elsif (Time.now < @stquiz.open_time || Time.now > @stquiz.close_time)
+          flash[:message] = "This quiz is not currently open"
+          erb :'/quiz/student/error'
+        else
+          erb :'/quiz/student/show'
+        end
+      else
+        redirect '/'
+      end
+    else
+      redirect '/'
+    end
+  end
+
 end
