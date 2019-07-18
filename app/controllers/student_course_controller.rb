44038fbae17a90c7f0c679c26aaa18e1cfb26c00
Jim <teachlife@outlook.com>
2019-07-18 13:46:00 -0400

    completes student course show page

--- app/controllers/student_course_controller.rb
+++ app/controllers/student_course_controller.rb
@@ -8,8 +8,7 @@ class StudentCourseController < ApplicationController
       if (Helper.current_student(session) == Student.find_by_slug(params[:slug]))
         @student = Helper.current_student(session)
         @course = Course.find(params[:id])
-        @studquiz = @student.student_quizzes
-        @quizzes = @studquiz.find_all{|obj| obj.course_quiz.course == @course}
+        @quizzes = @student.student_quizzes.find_all{|obj| obj.course_quiz.course == @course}
         erb :'course/student/show'
       else
         redirect '/'
