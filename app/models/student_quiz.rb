0edfdb83432b5a273dbbd6c3de408d93ca57c802
Jim <teachlife@outlook.com>
2019-07-21 15:47:06 -0400

    creates method to save student questions

--- app/models/student_quiz.rb
+++ app/models/student_quiz.rb
@@ -3,4 +3,11 @@ class StudentQuiz < ActiveRecord::Base
   belongs_to :quiz
   belongs_to :course_quiz
   has_many :student_questions
+
+  def save_student_answers(params)
+    params["question_id"].each do |qid|
+      question = Question.find(qid)
+      StudentQuestion.create(:student_quiz => self.id, :question => question.id, :student_answer => params["student_answers"][qid])
+    end
+  end
 end
